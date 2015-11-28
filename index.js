9ea519c004aecfeabf3c2956b7a67fb10260c0e0
Curran <curran.kelleher@gmail.com>
2015-11-28 15:30:20 -0800

    Start validation implementation - validate that the  property exists. #1

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-function ChiasmDataset(data, metadata){
-  return {
-    data: data,
-    metadata: metadata
-  };
+var strings = {
+  no_data_property: "The dataset.data property does not exist."
+};
+
+function error(id){
+  return Error(strings[id]);
 }
+
+module.exports = {
+  strings: strings,
+  validate: function (dataset){
+    return new Promise(function (resolve, reject){
+
+      // Validate that the `data` property exists.
+      if(!dataset.data){
+        reject(error("no_data_property"));
+      }
+    });
+  }
+};
--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-console.log("TODO implement tests for validation");
+var expect = require("chai").expect,
+    ChiasmDataset = require("./index"),
+    strings = ChiasmDataset.strings;
+
+describe("chiasm-dataset", function () {
+  it("should validate that the `data` property exists.", function(done) {
+    ChiasmDataset.validate({}).then(function (){}, function (err) {
+      expect(err).to.exist;
+      expect(err.message).to.equal(strings.no_data_property);
+      done();
+    });
+  });
+});
