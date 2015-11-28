9f1042e77e8d0e23168a67a3c87c0be635ea1791
Curran <curran.kelleher@gmail.com>
2015-11-28 15:52:40 -0800

    Validate that dataset.data is an array #1

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 var strings = {
-  no_data_property: "The dataset.data property does not exist."
+  no_data_property: "The dataset.data property does not exist.",
+  data_not_array: "The dataset.data property is not an array, its type is '%type%'."
 };
 
-function error(id){
-  return Error(strings[id]);
+function error(id, params){
+  return Error(errorMessage(id, params));
+}
+
+function errorMessage(id, params){
+  return template(strings[id], params);
+}
+
+// Simple templating from http://stackoverflow.com/questions/377961/efficient-javascript-string-replacement
+function template(str, params){
+  return str.replace(/%(\w*)%/g, function(m, key){
+    return params[key];
+  });
 }
 
 module.exports = {
   strings: strings,
+  errorMessage: errorMessage,
   validate: function (dataset){
     return new Promise(function (resolve, reject){
 
       // Validate that the `data` property exists.
       if(!dataset.data){
         reject(error("no_data_property"));
+
+      // Validate that the `data` property is an array.
+      } else if((typeof dataset.data) !== "Array"){
+        reject(error("data_not_array", { type: typeof dataset.data}));
+      } else {
+        resolve();
       }
     });
   }
--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 var expect = require("chai").expect,
     ChiasmDataset = require("./index"),
-    strings = ChiasmDataset.strings;
+    strings = ChiasmDataset.strings,
+    errorMessage = ChiasmDataset.errorMessage;
 
 describe("chiasm-dataset", function () {
+
   it("should validate that the `data` property exists.", function(done) {
     ChiasmDataset.validate({}).then(function (){}, function (err) {
       expect(err).to.exist;
@@ -10,4 +12,25 @@ describe("chiasm-dataset", function () {
       done();
     });
   });
+
+  it("should validate that the `data` property is an array of objects (reject string).", function(done) {
+    ChiasmDataset.validate({
+      data: "foo"
+    }).then(function (){}, function (err) {
+      expect(err).to.exist;
+      expect(err.message).to.equal(errorMessage("data_not_array", { type: "string" }));
+      done();
+    });
+  });
+
+  it("should validate that the `data` property is an array of objects (reject number).", function(done) {
+    ChiasmDataset.validate({
+      data: 5
+    }).then(function (){}, function (err) {
+      expect(err).to.exist;
+      expect(err.message).to.equal(errorMessage("data_not_array", { type: "number" }));
+      done();
+    });
+  });
+
 });
